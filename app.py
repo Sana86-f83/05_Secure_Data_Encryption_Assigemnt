27a04aed41e86707f55b90d5bb04c61cdd486431
Sana86-f83 <sana.faisalmehmood@gmail.com>
2025-04-22 12:24:49 +0500

    update

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ elif choice == "Register":
         else:
             st.error("Both fields are required.")
 
-    elif choice == "Login": 
+elif choice == "Login": 
         st.subheader("🔑 User Login")
 
         if time.time() < st.session_state.lockout_time:
